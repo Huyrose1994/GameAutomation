2fcfbfc4193e53201d1044ba7cf010ad3a7153b6
Huyrose1994 <huynguyen2912@gmail.com>
2024-02-13 23:56:20 +0700

    Update

--- automation.py
+++ automation.py
@@ -13,6 +13,9 @@ Take screenshot (every 1 second)
 Check Image:
     Yes: Click on Image
     No: Click on Quest
+        Check Image Q1:
+            Change: Continue
+            No Change: Click Image Q2
 Recursive Loop
 '''
 
@@ -36,12 +39,11 @@ class Automation():
         list_files = glob.glob('./DetectionObjects/*.png')
         for index, file in enumerate(list_files):
             self.dict_image[index] = cv2.imread(file)
-            print(cv2.imread(file))
         self.threshold = 0.7
 
         # Number of click
         self.n_clicks = 1000
-        self.list_quests = [(319,303)]
+        self.list_quests = [(319,303), (376,422)]
         self.list_screenshot = []
         self.previous_main = []
         self.i = 0
@@ -53,21 +55,20 @@ class Automation():
     def recursive_process(self):
         # Check current screen
         screen = self.take_screenshot(self.left, self.top, self.width, self.height)
-        main_quest = self.take_screenshot(225, 243, 531, 350)
+        main_quest = cv2.cvtColor(self.take_screenshot(225, 243, 531, 350), cv2.COLOR_BGR2GRAY)
         self.list_screenshot.append(screen)
         self.previous_main.append(main_quest)
         if len(self.list_screenshot) > 3:
             del self.list_screenshot[0]
         if len(self.previous_main) > 3:
             del self.previous_main[0]
-        print(len(self.previous_main))
 
         # Check image
         for key,value in self.dict_image.items():     
             similarity = cv2.matchTemplate(screen, value, cv2.TM_CCOEFF_NORMED)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(similarity)
             bottom_right = (max_loc[0] + 180, max_loc[1] + 50)  # (Where w, h are dimensions of the template)
-        
+ 
             if max_val >= 0.95: # Check similarity & click on image
                 pyautogui.click(bottom_right)
                 sleep(0.5)
@@ -75,52 +76,19 @@ class Automation():
                 try:
                     score, _ = structural_similarity(self.previous_main[-1], self.previous_main[-2], full = True)
                     print(score)
+                    if score >= 0.8:
+                        self.xloc, self.yloc = self.list_quests[-1]
+                        pyautogui.leftClick(x = self.xloc, y = self.yloc)
+                        sleep(5)
+                    else:
+                        self.xloc, self.yloc = self.list_quests[0]
+                        pyautogui.leftClick(x = self.xloc, y = self.yloc)
+                        sleep(5)
                 except:
-                    print('Failure')
-                    # try:
-                    #     print(self.previous_main[-1])
-                    #     print(self.previous_main[-2])
-                    # except:
+                    print('Not Enough Data: Failure')
                     pass
-                self.xloc, self.yloc = self.list_quests[-self.i]
-                pyautogui.leftClick(x = self.xloc, y = self.yloc)
-                sleep(5)
+
             self.recursive_process()
-
-
-
-    # def skip_quest(self):
-    #     '''
-    #     Check if the screen doesn't change that much after 3 seconds
-    #     '''
-
-    #     # Click second quest if first quest doesn't move
-    #     screenshot = self.take_screenshot()
-    #     self.list_screenshot.append(screenshot)
-    #     __corr = cv2.matchTemplate(screenshot, self.list_screenshot[-1], cv2.TM_CCOEFF_NORMED)
-    #     # Do multiple quests
-    #     if __corr > 0.999:
-    #         self.i += 1
-    #         self.xloc, self.yloc = self.list_quests[-self.i]
-    #         print(self.i)
-        
-    #     # Only do the first quest
-    #     self.xloc, self.yloc = self.list_quests[0]
-    #     result_1 = cv2.matchTemplate(screenshot, self.image_1, cv2.TM_CCOEFF_NORMED)
-    #     result_2 = cv2.matchTemplate(screenshot, self.image_2, cv2.TM_CCOEFF_NORMED)
-
-    #     pyautogui.leftClick(x = self.xloc, y = self.yloc)
-    #     print('Click Quest', end = '\r')
-    #     self.click_on_result(result_1)
-    #     self.click_on_result(result_2)     
-    #     sleep(5)
-    #     self.skip_quest()
-    
-    # def start(self):
-    #     while self.i < self.n_clicks:
-    #         pyautogui.leftClick(x = self.xloc, y = self.yloc)
-
-
 
 if __name__ == '__main__':
      print('Running')
